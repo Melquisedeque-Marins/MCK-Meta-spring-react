41f94239feb1365ac80312c0b95fdfc3dff26234
melck <melck_junior@hotmail.com>
2022-07-12 10:54:45 +0100

    Project create

---
+++ backend/src/main/java/com/melck/mckmeta/MckmetaApplication.java
@@ -0,0 +1,13 @@
+package com.melck.mckmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MckmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MckmetaApplication.class, args);
+	}
+
+}
