a8ab729069698bbdd287e1d5ad9c209c834b4cae
melck <melck_junior@hotmail.com>
2022-07-13 18:46:03 +0100

    Date select

--- backend/src/main/java/com/melck/mckmeta/controllers/SaleController.java
+++ backend/src/main/java/com/melck/mckmeta/controllers/SaleController.java
@@ -1,11 +1,12 @@
 package com.melck.mckmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.melck.mckmeta.entities.Sale;
@@ -13,14 +14,17 @@ import com.melck.mckmeta.services.SaleService;
 
 @RestController
 @RequestMapping(value = "/sales")
-public class SasleController {
+public class SaleController {
 
     @Autowired
     private SaleService service;
 
     @GetMapping
-    public ResponseEntity<List<Sale>> findSales(){
-        return ResponseEntity.ok().body(service.findSales());
+    public ResponseEntity<Page<Sale>> findSales(
+        @RequestParam(value = "minDate", defaultValue = "") String minDate,
+        @RequestParam(value = "maxDate", defaultValue = "")String maxDate,
+        Pageable pageable){
+        return ResponseEntity.ok().body(service.findSales(minDate, maxDate, pageable));
         
     }
     
